4eeee9e5f7cae5209e373ae828523fc3d27dd40a
idan <idan@soluto.com>
2017-08-28 18:44:52 +0300

    observe the content view's sublayer

--- ios/LockableScrollView/RNLockableScrollView.h
+++ ios/LockableScrollView/RNLockableScrollView.h
@@ -2,9 +2,6 @@
 
 @interface RNLockableScrollView : RCTScrollView
 
-//@property (nonatomic, readonly) UIScrollView *scrollView;
-//@dynamic scrollView;
-- (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
 - (void)scrollTo:(int)x y:(int)y animated:(BOOL)animated;
 
 @end
--- ios/LockableScrollView/RNLockableScrollView.m
+++ ios/LockableScrollView/RNLockableScrollView.m
@@ -1,28 +1,31 @@
 #import "RNLockableScrollView.h"
-#import "RCScrollView.h"
+#import <React/UIView+React.h>
+//@interface RNLockableScrollView : RCTScrollView
+//
+////@property (nonatomic, readonly) UIScrollView *scrollView;
+////@dynamic scrollView;
+//- (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
+//- (void)scrollTo:(int)x y:(int)y animated:(BOOL)animated;
+//
+//@end
 
 @implementation RNLockableScrollView
 
 @dynamic scrollView;
 
-- (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher 
+- (void)insertReactSubview:(UIView *)view atIndex:(NSInteger)atIndex
 {
-    if (self = [super initWithEventDispatcher:eventDispatcher]) {
-        UIScrollView *scrollView = [super scrollView];
-        //        scrollView.addObserver(self, forKeyPath: "contentSize", options: [.new, .old], context: nil)
-
-        [scrollView addObserver:self forKeyPath:@"subviews" options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context: nil];
-        return self;
-    }
-
-    return nil;
+  [super insertReactSubview:view atIndex:atIndex];
+  UIScrollView *scrollView = [super scrollView];
+  UIView *contentView = [scrollView subviews][0];
+  [contentView addObserver:self forKeyPath:@"layer.sublayers" options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context: nil];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath
                       ofObject:(id)object
                         change:(NSDictionary *)change
                        context:(void *)context {
-    
+    NSLog(@"observeValueForKeyPath %@ with change %@", keyPath, change);
     if ([keyPath  isEqual: @"subviews"]) {
       NSArray *oldSubviews = [change objectForKey:@(NSKeyValueObservingOptionOld)];
       NSLog(@"oldSubviews %@", oldSubviews);
@@ -36,6 +39,10 @@
     NSLog(@"scrollTo %d %d", x, y);
     UIScrollView *scrollView = [super scrollView];
     [scrollView setContentOffset:CGPointMake(x, y) animated:animated];
+}
+
+- (void)didAddSubview:(UIView *)subview {
+  NSLog(@"scrollTo %@", subview);
 }
 
 //    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
