d0888d20e9affc4b5d6a7a695b27d29cf344251c
idan <idan@soluto.com>
2017-09-03 18:40:22 +0300

    remove unused

--- ios/LockableScrollView/RCScrollView.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface RCScrollView : UIScrollView
-
-@property BOOL enabled;
-- (void)didAddSubview:(UIView *)subview;
-- (void)willRemoveSubview:(UIView *)subview;
-
-@end
